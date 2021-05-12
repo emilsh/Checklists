9de293b23e6941c4f337b43078ff7ff6d285fdad
Emil Shafigin <emilshafigin@Emils-MacBook-Pro.local>
2021-05-13 00:48:47 +0600

    Adding an informationabout the number of to-do items remaining for each list

--- Checklists/Model/Checklist.swift
+++ Checklists/Model/Checklist.swift
@@ -10,23 +10,28 @@ import UIKit
 
 class Checklist: NSObject, NSCoding {
     
-    var name = ""
-    var items = [ChecklistItem]()
+  var name = ""
+  var items = [ChecklistItem]()
     
-    init(name: String) {
-        self.name = name
-        super.init()
+  init(name: String) {
+    self.name = name
+    super.init()
+  }
+    
+  func encode(with aCoder: NSCoder) {
+    aCoder.encode(name, forKey: "Name")
+    aCoder.encode(items, forKey: "Items")
+  }
+    
+  required init?(coder aDecoder: NSCoder) {
+    name = aDecoder.decodeObject(forKey: "Name") as! String
+    items = aDecoder.decodeObject(forKey: "Items") as! [ChecklistItem]
+    super.init()
+  }
+  
+  func countUncheckedItems() -> Int {
+    return items.reduce(0) { cnt, item in
+      cnt + (item.checked ? 0 : 1)
     }
-    
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(name, forKey: "Name")
-        aCoder.encode(items, forKey: "Items")
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        name = aDecoder.decodeObject(forKey: "Name") as! String
-        items = aDecoder.decodeObject(forKey: "Items") as! [ChecklistItem]
-        super.init()
-    }
-    
+  }
 }
--- Checklists/Model/ChecklistItem.swift
+++ Checklists/Model/ChecklistItem.swift
@@ -10,26 +10,26 @@ import Foundation
 
 class ChecklistItem: NSObject, NSCoding {
     
-    func encode(with aCoder: NSCoder) {
+  var text = ""
+  var checked = false
+  
+  func encode(with aCoder: NSCoder) {
         aCoder.encode(text, forKey: "Text")
         aCoder.encode(checked, forKey: "Checked")
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        text = aDecoder.decodeObject(forKey: "Text") as! String
-        checked = aDecoder.decodeBool(forKey: "Checked")
+  required init?(coder aDecoder: NSCoder) {
+      text = aDecoder.decodeObject(forKey: "Text") as! String
+      checked = aDecoder.decodeBool(forKey: "Checked")
         
-        super.init()
-    }
+      super.init()
+  }
     
-    override init() {
-        super.init()
-    }
+  override init() {
+    super.init()
+  }
     
-    var text = ""
-    var checked = false
-    
-    func toggleChecked() {
-        checked = !checked
-    }
+  func toggleChecked() {
+      checked = !checked
+  }
 }
